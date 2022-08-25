9e0333cf2474c646c5c98f9c7ff9d5eb7c0f6bac
Matheus Cruz <matheusd20m@gmail.com>
2022-08-25 12:53:35 -0300

    Banco de dados Postgresql e base de dados

--- src/main/java/com/matheuscruz/cadastro/domain/Pedidos.java
+++ src/main/java/com/matheuscruz/cadastro/domain/Pedidos.java
@@ -5,15 +5,33 @@ import java.sql.Timestamp;
 import java.util.Objects;
 import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Table(name= "pedidos")
+@Entity
 public class Pedidos implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
 	
-	
+	@Column
 	private Integer number;
+	
+	@Column
 	private Timestamp date;
+	
+	@Column
 	private double percentualDiscount;
+	
+	@Column
 	private double totalValue;
 	
 	
